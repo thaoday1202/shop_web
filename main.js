0cff4286e29ebd773425dfa6eecdf55b12879b31
thaoday1202 <thaoday1202@gmail.com>
2022-06-01 09:10:08 +0700

    nothing

---
+++ main.js
@@ -0,0 +1,36 @@
+
+// function myFunction () {
+//     document.getElementById("hello_Js").innerHTML = alert('hello bro') ;
+// }
+
+// var result = 'A' && 'B';
+
+// if (result) {
+//     console.log("dung");
+// } else {
+//     console.log('sai');
+// }
+
+// var myInfo = {
+//     name: 'han nah',
+//     age: 18,
+//     address: 'Ha Noi'
+// }
+
+// console.log(myInfo.name);
+
+
+// function User ( firstName, lastName, avatar) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.avatar = avatar;
+// }
+
+// var author = new User('han nah', 'hann', 'img');
+
+// console.log(author);
+
+
+for (var i =100; i >= 0; i -=5 ) {
+    console.log(i + ' ' + 'hello');
+}
